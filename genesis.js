533cf14973100680cd7362fb50efad51e9fb8b34
ririririririn <uijkm159@naver.com>
2023-03-14 18:13:55 +0900

    23.03.14

--- genesis.js
+++ genesis.js
@@ -1,11 +1,11 @@
-const btnHamburger = document.querySelector('.hamburger');
-const btnHamburgerLine1 = document.querySelector('.line1');
-const btnHamburgerLine2 = document.querySelector('.line2');
+const btnHamburger = document.querySelector(".hamburger");
+const btnHamburgerLine1 = document.querySelector(".line1");
+const btnHamburgerLine2 = document.querySelector(".line2");
 
-function activeHamburger(){
-  btnHamburgerLine1.classList.toggle('active_hbgbtn1');
-  btnHamburgerLine2.classList.toggle('active_hbgbtn2');
+function activeHamburger() {
+  btnHamburgerLine1.classList.toggle("active_hbgbtn1");
+  btnHamburgerLine2.classList.toggle("active_hbgbtn2");
   console.log("??");
 }
 
-btnHamburger.addEventListener('click',activeHamburger);
+btnHamburger.addEventListener("click", activeHamburger);
